040445f7b3e3150003bd0521faa56833aa04de8b
Adan Rabadan <evilb4k4@gmail.com>
2017-05-18 00:12:05 -0700

    added more content

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 'use strict';
-function Project() {
-  this.name = name;
-  this.author = author;
+function Project (title, content, link, deployDate, lastUpdated, screenshot) {
+  this.title = title;
+  this.content = content;
+  this.link = link;
+  this.deployDate = deployDate;
+  this.lastUpdated = lastUpdated;
+  this.screenshot = screenshot;
 }
+$(document).ready(function(){
+  var $newProject = $('<li>' + new Project + '</li>');
+  $('projectList').prepend('$newProject');
+});
