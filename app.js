f44d8d77f01acd3fdd332af396be464d0b661d2d
Adan.Rabadan <evilb4k4@gmail.com>
2017-06-04 17:52:01 -0700

    added data for project

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 'use strict';
-function Project (title, content, link, deployDate, lastUpdated, screenshot) {
-  this.title = title;
-  this.content = content;
-  this.link = link;
-  this.deployDate = deployDate;
-  this.lastUpdated = lastUpdated;
-  this.screenshot = screenshot;
+
+var projects = [];
+
+function Project (items) {
+  this.title = items.title;
+  this.link = items.link;
+  this.deployDate = items.deployDate;
+  this.lastUpdated = items.lastUpdated;
+  this.screenshot = items.screenshot;
+  this.description = items.description;
 }
-$(document).ready(function(){
-  var $newProject = $('<li>' + new Project + '</li>');
-  $('projectList').prepend('$newProject');
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+
+  $newProject.find('.title').html(this.title);
+  $newProject.find('.projectLink').html(this.link);
+  $newProject.find('.deploy').html(this.deployDate);
+  $newProject.find('.updated').html(this.lastUpdated);
+  $newProject.find('.projectDescription').html(this.description);
+  return $newProject;
+};
+
+projectData.sort(function(a,b) {
+  return (new Date(b.deployDate)) - (new Date(a.deployDate));
+});
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+  console.log(projects);
+});
+
+projects.forEach(function(a) {
+  console.log(a);
+  $('#projectPlaceholder').append(a.toHtml());
 });
